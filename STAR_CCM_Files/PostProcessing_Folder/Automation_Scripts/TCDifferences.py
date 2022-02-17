e583fc06c566c0e1555ca5ee8b36462b644f1787
JoshuaHalsted <71159643+JoshuaHalsted@users.noreply.github.com>
2022-02-16 18:10:01 -0800

    Altered the output .txt file

--- STAR_CCM_Files/PostProcessing_Folder/Automation_Scripts/TCDifferences.py
+++ STAR_CCM_Files/PostProcessing_Folder/Automation_Scripts/TCDifferences.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import os
 
 def ImportStuff(filename):
     import_df = pd.read_csv(filename)
@@ -23,14 +24,22 @@ TC_RESULTS = ImportStuff(TC_DATA_FILE)
 
 TC_List = STAR_RESULTS.columns
 
-print("Mesh_Setting_1:")
+string_list = []
+string_list.append("Mesh_Setting_1:")
 for column in STAR_RESULTS:
     try:
         ABS_DIFF = float(TC_RESULTS[column][0])+273.15-float(STAR_RESULTS[column][0])
-        REL_DIFF = 100*  ABS_DIFF/(float(TC_RESULTS[column][0])+273.15)
-        #print("Difference is %s"%(TC_RESULTS[column][0]-STAR_RESULTS[column][0]))
-        print("\t%s:"%(column))
-        print("\t\tAbsolute difference is %s Kelvin\n"%(np.round(ABS_DIFF,4)))
-        print("\t\tRelative difference is %s percent\n"%(np.abs(np.round(REL_DIFF,4))))
+        REL_DIFF = 100* ABS_DIFF/(float(TC_RESULTS[column][0])+273.15)
+        string_list.append("\t%s:"%(column))
+        string_list.append("\t\tPG-28 result: %s Kelvin"%(np.round(float(TC_RESULTS[column][0])+273.15,5)))
+        string_list.append("\t\tSTAR result: %s Kelvin"%(np.round(float(STAR_RESULTS[column][0]),5)))
+        string_list.append("\t\tAbsolute difference is %s Kelvin"%(np.round(ABS_DIFF,4)))
+        string_list.append("\t\tRelative difference is %s percent"%(np.abs(np.round(REL_DIFF,4))))
     except:
         pass
+
+#print(string_list)
+with open(os.path.join("C:/Users/17577/Thesis_Work/STAR_CCM_Files/PostProcessing_Folder/Automation_Scripts",'STAR_LP_TC_Results.txt'), 'w') as f:
+    for line in string_list:
+        f.write(line)
+        f.write('\n')
