ab7d7a2cf2a1142345c916bf03d3ce1c67a35231
JoshuaHalsted <71159643+JoshuaHalsted@users.noreply.github.com>
2022-02-16 17:57:21 -0800

    Implemented separate flow regions

--- Other_Calculations/GCI_Study/GCI_Script.py
+++ Other_Calculations/GCI_Study/GCI_Script.py
@@ -11,7 +11,8 @@ class Simulation():
     def __init__(self, sim_dict):
         self._name = sim_dict['Name']
         self._num_cells = sim_dict['Number_Cells']
-        self._char_size = (TOTAL_SIM_VOLUME/self._num_cells)**(1/3)
+        #self._char_size = (TOTAL_SIM_VOLUME/self._num_cells)**(1/3)
+        self._char_size = (TOTAL_SIM_VOLUME/self._num_cells)**(1)
 
 def CreateRatio(Sim1, Sim2):
     return Sim1._char_size/Sim2._char_size
@@ -20,7 +21,9 @@ def CompareRatios(Sim_List):
     iteration = 0
     ratio_list = []
     while True:
-        if iteration < (len(Sim_List)-1):
+        print(len(Sim_List))
+        #if iteration < (len(Sim_List)-1):
+        if iteration < 1:
             ratio_list.append(CreateRatio(Sim_List[iteration+1], Sim_List[iteration]))
             if ratio_list[iteration] < 1.3:
                 print("Grid %s needs to be refined"%(Sim_List[iteration+1]._name))
@@ -33,17 +36,18 @@ def CompareRatios(Sim_List):
 
 def main():
     with open(STAR_RESULTS_YAML) as fh:
-        Analysis_Dict = yaml.load(fh, Loader=yaml.FullLoader)['Analysis']
+        Analysis_Dict = yaml.load(fh, Loader=yaml.FullLoader)
     simulation_list = []
-    Sim_List = []
-    for item in Analysis_Dict:
-        if item == 'Fluid_Volume':
-            Fluid_Volume = Analysis_Dict['Fluid_Volume']
-        else:
-            Sim_Instance = Simulation(Analysis_Dict[item])
-            Sim_List.append(Sim_Instance)
-    Sim_List.append(Sim_List[0])
-    ratio_list = CompareRatios(Sim_List)
-    print(ratio_list)
+    for region in Analysis_Dict.keys():
+        Sim_List = []
+        for item in Analysis_Dict[region]:
+            if item == 'Fluid_Volume':
+                Fluid_Volume = Analysis_Dict[region]['Fluid_Volume']
+            else:
+                Sim_Instance = Simulation(Analysis_Dict[region][item])
+                Sim_List.append(Sim_Instance)
+        Sim_List.append(Sim_List[0])
+        ratio_list = CompareRatios(Sim_List)
+        print(ratio_list)
 
 main()
